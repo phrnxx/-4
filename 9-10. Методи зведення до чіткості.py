27c8bb838ec07175bf1d4d8e7c1f888064f0037d
frnxx <144995075+phrnxx@users.noreply.github.com>
2025-03-04 08:25:55 +0200

    9-10. Методи зведення до чіткості.py

--- 9-10. Методи зведення до чіткості.py
+++ 9-10. Методи зведення до чіткості.py
@@ -2,7 +2,7 @@ import skfuzzy as fuzz
 import numpy as np
 
 x = np.arange(0, 11, 1)
-y = fuzz.trimf(x, [0, 5, 10]) 
+y = fuzz.trimf(x, [0, 5, 10])  
 
-центроїд = fuzz.defuzz(x, y, 'centroid')
-print(f"Чітке значення (центроїд): {центроїд:.2f}")
+centroid = fuzz.defuzz(x, y, 'centroid')
+print(f"Чітке значення (центроїд): {centroid:.2f}")
